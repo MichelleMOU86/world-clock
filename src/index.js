42dc74c97fb96d100611220bf1e3c271e4068ef5
MichelleMOU86 <Michelle.L.Munoz@gmail.com>
2024-12-11 21:32:46 -0500

    Added a third city to home page

--- src/index.js
+++ src/index.js
@@ -16,6 +16,14 @@ function updateTime() {
   parisTimeElement.innerHTML = `${parisTime.format(
     "h:mm:ss [<small>]A[</small>]"
   )}`;
+  let tokyoElement = document.querySelector("#tokyo");
+  let tokyoDateElement = tokyoElement.querySelector(".date");
+  let tokyoTimeElement = tokyoElement.querySelector(".time");
+  let tokyoTime = moment().tz("Asia/Tokyo");
+  tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
+  tokyoTimeElement.innerHTML = `${tokyoTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  )}`;
 }
 setInterval(updateTime, 1000);
 
